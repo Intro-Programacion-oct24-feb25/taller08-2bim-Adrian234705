a97428ba29e170fcc16597c6093edeb078b000b8
Adrian <arurgiles2@utpl.edu.ec>
2024-12-05 18:53:21 -0500

    Taller08

--- Taller08/src/problemas/Problema02.java
+++ Taller08/src/problemas/Problema02.java
@@ -4,6 +4,9 @@
  */
 package problemas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -14,7 +17,21 @@ public class Problema02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner (System.in);
+           entrada.useLocale(Locale.US); 
+           int contador = 0; 
+           double farenheit ;
+           double celcius; 
+           String cadena = ""; 
+           
+           for(farenheit = 20; farenheit <= 100; farenheit += 4){
+               celcius = (5.0/9.0)*(farenheit - 32); 
+               cadena = String.format("%s\nOperacion(%d) Grados Farenheit: %.2f ===> Grados Celcius: %.2f",
+                       cadena,contador,farenheit,celcius);
+               
+           }
+           System.out.println(cadena); 
+           
     }
     
 }
--- Taller08/src/problemas/Problema03.java
+++ Taller08/src/problemas/Problema03.java
@@ -14,7 +14,21 @@ public class Problema03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for(int i = 1; i <= 5; i++){
+            for(int contador = 1; contador <= i; contador++){
+                
+                System.out.printf("%s","*");
+            }
+            System.out.println();
+            
+        }
+        for (int n = 5; n >= 1; n-- ){
+            for (int contador2 = 1; contador2 <= n; contador2++){
+                System.out.printf("%s","*");
+            }
+            System.out.println(); 
+            }
+        
     }
     
 }
